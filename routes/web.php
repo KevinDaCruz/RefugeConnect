2c18f124ccc209ba2d53802f77711118434eff00
KevinDaCruz <kevindc39@hotmail.com>
2026-03-13 10:04:57 +0100

    Activité de synthèse : fix 3

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index'])->name('animals.index');
 
 Route::prefix('animaux')->name('animals.')->group(function () {
-    Route::get('/', [IndexController::class, 'index'])->name('list');
     Route::get('/ajouter', [AnimalController::class, 'add'])->name('create');
     Route::get('/{animal}', [AnimalController::class, 'show'])->name('show');
     Route::get('/{animal}/modifier', [AnimalController::class, 'edit'])->name('edit');
